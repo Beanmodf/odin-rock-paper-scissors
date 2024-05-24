7d5a8d005340fc9a9144b60e74d7f6d7816217a8
Beanmodf <dominhtriet47@gmail.com>
2024-05-24 22:04:29 +0700

    Create UI and regulate javascript to operate rock-paper-scissors game

--- source.js
+++ source.js
@@ -1,76 +1,68 @@
+const resultPar = document.querySelector(".result");
+const humanPar = document.querySelector(".human-decision");
+const computerSelectionPar = document.querySelector(".computer-decision");
+
+let computerSelection = null;
+let humanSelection = null;
+
+const decisionCon = document.querySelector(".decision-container");
+
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function getHumanChoice() {
-    return prompt("Which decision you are going to make?");
-}
-
-function proccessHumanChoice(str) {
-    str = str.trim();
-    str = str.toLowerCase();
-
-    if (str === "rock") {
-        return 0;
-    } else if (str === "paper") {
-        return 1; 
+function proccessChoice(number) {
+    if (number === 0) {
+        return "Rock";
+    } else if (number === 1) {
+        return "Paper";
     } else {
-        return 2;
+        return "Scissors";
     }
 }
 
-function incHumanScore() {
-    humanScore++;
+function playground(num1, num2) {
+    let result = num1 - num2;
+    
+    computerSelectionPar.textContent = proccessChoice(computerSelection); 
+    humanPar.textContent = proccessChoice(humanSelection);
+
+    if (result === 1 || result === -2) {
+        resultPar.textContent = "You win!"; 
+    } else if (result === -1 || result === 2) {
+        resultPar.textContent = "You loose!"
+    } else {
+        resultPar.textContent = "Tied game!"
+    }
 }
 
-function incComputerScore(){
-    computerScore++;
-}
-
-let humanScore = 0;
-let computerScore = 0;
 function playgame() {
-    const computerSelection = getComputerChoice();
-    const humanSelection = proccessHumanChoice(getHumanChoice());
+    resultPar.textContent = "";
+    computerSelectionPar.textContent = "";
+    humanPar.textContent = "";
 
-    function playround(human, computer) {
-        const result = human - computer;
+    computerSelection = getComputerChoice();    
 
-        let humanContext = "";
-        let computerContext = "";
-
-        if (human === 0) {
-            humanContext =  "Rock";
-        } else if (human === 1) {
-            humanContext = "Paper";
-        } else {
-            humanContext = "Scissors";
-        }
-
-        if (computer === 0) {
-            computerContext =  "Rock";
-        } else if (computer === 1) {
-            computerContext = "Paper";
-        } else {
-            computerContext = "Scissors";
-        }
-
-        if (result === 1 || result === -2) {
-            console.log("You win! " + humanContext + " beats " + computerContext); 
-            humanScore++;
-        } else if (result === -1 || result === 2) {
-            console.log("You loose! " + computerContext + " beats " + humanContext);
-            computerScore++;
-        } else {
-            console.log("You tied. You both are: " + computerContext  );
-        };
-    }
-    playround(humanSelection, computerSelection)
-    console.log("Your score: "+ humanScore);
-    console.log("Computer score: " + computerScore);
+    decisionCon.addEventListener("click", (e) => {
+        switch (e.target.id) {
+            case "rock-btn": 
+                humanSelection = 0;
+                break;
+            case "paper-btn":
+                humanSelection = 1;
+                break;
+            case "scissors-btn":
+                humanSelection = 2;
+                break;
+            default:
+                break;
+        } 
+        playground(humanSelection, computerSelection);
+    })
 }
 
-for (let i = 0 ; i < 5; i++) {
-    playgame()
-}
+const startBtn = document.querySelector(".start-btn");
+
+startBtn.addEventListener("click", playgame);
+
 
